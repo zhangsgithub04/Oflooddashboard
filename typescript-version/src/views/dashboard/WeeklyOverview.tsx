18ca994f90c090fa8845a8a7e5df75dae547e5cb
zhangsgithub04 <ssen_zhang@hotmail.com>
2024-01-01 15:28:33 -0500

    Update WeeklyOverview.tsx

--- typescript-version/src/views/dashboard/WeeklyOverview.tsx
+++ typescript-version/src/views/dashboard/WeeklyOverview.tsx
@@ -86,7 +86,7 @@ const WeeklyOverview = () => {
   return (
     <Card>
       <CardHeader
-        title='Weekly Overview on the Flood Data in Otsego County!'
+        title='Weekly Overview on the Usersage!'
         titleTypographyProps={{
           sx: { lineHeight: '2rem !important', letterSpacing: '0.15px !important' }
         }}
